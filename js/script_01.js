b088a62d7bd6f21de0090c669c0c5344df01c323
unknown <emal715@naver.com>
2021-02-03 11:22:17 +0900

    update 2021-02-03 11:22

---
+++ js/script_01.js
@@ -0,0 +1,7 @@
+//script_01.js
+//자바스크립트 처음 코딩하기
+//ECMA Script 5버전 ''
+console.log('ECMA Script 5 자바스크립트 처음 코딩하기 & node_js 로실행하기!!!');
+
+//ECMA Script 6버전 ``
+console.log(`ECMA Script 6 자바스크립트 처음 코딩하기 & node_js 로실행하기!!!`);
